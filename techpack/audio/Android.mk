0a53bb7b12c3c845867358a48ce656c70bf45f8f
Arian <arian.kulmer@web.de>
2021-04-29 20:06:28 +0200

    asoc: codecs: Import awinic codec
    Change-Id: I8037bcedf666a1df9657f002dc0ff006ba42e9e3

--- techpack/audio/Android.mk
+++ techpack/audio/Android.mk
@@ -14,6 +14,7 @@ $(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/Modul
 $(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/codecs/Module.symvers)
 $(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/codecs/wcd934x/Module.symvers)
 $(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/codecs/tfa98xx/Module.symvers)
+$(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/codecs/awinic/Module.symvers)
 
 include $(MY_LOCAL_PATH)/include/uapi/Android.mk
 include $(MY_LOCAL_PATH)/ipc/Android.mk
@@ -24,6 +25,7 @@ include $(MY_LOCAL_PATH)/asoc/Android.mk
 include $(MY_LOCAL_PATH)/asoc/codecs/Android.mk
 include $(MY_LOCAL_PATH)/asoc/codecs/wcd934x/Android.mk
 include $(MY_LOCAL_PATH)/asoc/codecs/tfa98xx/Android.mk
+include $(MY_LOCAL_PATH)/asoc/codecs/awinic/Android.mk
 endif
 
 ifeq ($(call is-board-platform-in-list, atoll),true)
