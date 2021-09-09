a2870ff3eb10d812bc52bdf961135976d4813538
daniml3 <danimoral1001@gmail.com>
2021-09-09 12:19:00 +0200

    drivers: gpu: dsi-staging: Implement gamma mode changes
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I58596ba0cdaf2621a03cb5dd82da041dde883fed

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.h
@@ -719,4 +719,7 @@ struct dsi_display *get_main_display(void);
 
 void dsi_display_set_fod_ui(struct dsi_display *display, bool status);
 
+void dsi_display_panel_gamma_mode_change(struct dsi_display *display,
+			struct dsi_display_mode *adj_mode, bool force);
+
 #endif /* _DSI_DISPLAY_H_ */
--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -258,6 +258,8 @@ struct dsi_panel {
 
 	struct brightness_alpha_pair *fod_dim_lut;
 	u32 fod_dim_lut_count;
+
+	int current_gamma;
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
@@ -387,5 +389,8 @@ int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status);
 u32 dsi_panel_get_fod_dim_alpha(struct dsi_panel *panel);
 
 int dsi_panel_apply_hbm_mode(struct dsi_panel *panel);
+
+void dsi_panel_gamma_mode_change(struct dsi_panel *panel,
+            struct dsi_display_mode *adj_mode, bool force);
 
 #endif /* _DSI_PANEL_H_ */
